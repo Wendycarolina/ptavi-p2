fbd3ae4e47fddd3e5429fe91615163e418259735
wendy carolina iza gualiche <wendy@zeta40.aulas.gsyc.es>
2015-09-29 21:49:33 +0200

    calc

--- calc.py
+++ calc.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Vamos a programar una calculadora
+#git commit -m "..." calc.py
+
 import sys
 
+def main():
+    pass
 
-def plus(op1, op2):
-    """ Function to sum the operands """
-    return op1 + op2
+def suma(sumando1, sumando2):
+	return float(sumando1) + float(sumando2) 
 
+def resta(numero1, numero2):
+	return float(numero1) - float(numero2)
 
-def minus(op1, op2):
-    """ Function to substract the operands """
-    return op1 - op2
 
 if __name__ == "__main__":
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+	try:
+	
+		operando1 = sys.argv[1]
+		operacion = str(sys.argv[2])
+		operando2 = sys.argv[3]
 
-    if sys.argv[2] == "suma":
-        result = plus(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        result = minus(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
 
-    print(result)
+	if operacion == "suma":
+		print(suma(operando1,operando2))
+	elif operacion == "resta":
+		print(resta(operando1,operando2))
+	else:
+		print("Operación incorrecta")
+
