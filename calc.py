cdf1d44b2d57d339e16517ea67bccedbdaa395f2
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-10-04 13:15:47 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Vamos a programar una calculadora
+#git commit -m "..." calc.py
+
 import sys
+
 
 def main():
     pass
 
+
 def suma(sumando1, sumando2):
-	return float(sumando1) + float(sumando2) 
+    return float(sumando1) + float(sumando2)
+
 
 def resta(numero1, numero2):
-	return float(numero1) - float(numero2)
+    return float(numero1) - float(numero2)
 
 
 if __name__ == "__main__":
-	try:
-	
-		operando1 = sys.argv[1]
-		operacion = str(sys.argv[2])
-		operando2 = sys.argv[3]
+    try:
+        operando1 = sys.argv[1]
+        operacion = str(sys.argv[2])
+        operando2 = sys.argv[3]
 
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-
-	if operacion == "suma":
-		print(suma(operando1,operando2))
-	elif operacion == "resta":
-		print(resta(operando1,operando2))
-	else:
-		print("Operación incorrecta")
-
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    if operacion == "suma":
+        print(suma(operando1, operando2))
+    elif operacion == "resta":
+        print(resta(operando1, operando2))
+    else:
+        print("Operación incorrecta")
