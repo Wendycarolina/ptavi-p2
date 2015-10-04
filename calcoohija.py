e986c1b95b144da3cbb904d741de595be15a179b
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-10-04 11:25:07 +0200

    Update calcoohija.py

--- calcoohija.py
+++ calcoohija.py
@@ -4,22 +4,22 @@
 import sys
 
 class Calculadora():
-	def __init__(self,num1,num2):
+	def __init__(self, num1, num2):
 		self.num1 = num1
 		self.num2 = num2
 
-	def suma(self,num1, num2):
+	def suma(self, num1, num2):
 		return num1 + num2 
 		
-	def resta(self,num1, num2):
+	def resta(self, num1, num2):
 		return num1 - num2
 
 class CalculadoraHija(Calculadora):
 
-	def multiplica(self,num1, num2):
+	def multiplica(self, num1, num2):
 		return num1*num2	
 
-	def divide(self,num1, num2):
+	def divide(self, num1, num2):
 		return num1 / num2
 
 if __name__ == "__main__":
@@ -36,13 +36,13 @@ if __name__ == "__main__":
 		sys.exit("Division by zero is not allowed")
 
 	if operacion == "suma":
-		print(x.suma(numero1,numero2))
+		print(x.suma(numero1, numero2))
 	elif operacion == "resta":
-		print(x.resta(numero1,numero2))
+		print(x.resta(numero1, numero2))
 	elif operacion == "multiplica":
-		print(x.multiplica(numero1,numero2))
+		print(x.multiplica(numero1, numero2))
 	elif operacion == "divide":
-		print(x.divide(numero1,numero2))
+		print(x.divide(numero1, numero2))
 	else:
 		print("Operación incorrecta")
 
