c00e06cad856d415771ef24de80d249f3e11e3a2
Wendycarolina <carolina.iza.g@gmail.com>
2015-10-04 13:32:56 +0200

    calcplusplus

---
+++ calcplusplus.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+
+
+class Calculadora():
+    def __init__(self, numeros):
+        self.numeros = numeros
+
+    def suma(self, sumandos):
+        a = []
+        for i in sumandos:
+            if i != sumandos[0]:
+                a.append(int(i))
+        print(a)
+        sumas = 0
+        for j in a:
+            sumas += j
+        print(sumas)
+
+    def resta(self, numeros):
+        a = []
+        for i in numeros:
+            if i != numeros[0]:
+                a.append(int(i))
+        print(a)
+        restas = 0
+        for j in a:
+            if j == a[0]:
+                j = j*-1
+            restas -= j
+        print(restas)
+
+
+class CalculadoraHija(Calculadora):
+    def multiplica(self, numeros):
+        a = []
+        for i in numeros:
+            if i != numeros[0]:
+                a.append(int(i))
+        print(a)
+        multiplicar = 1
+        for j in a:
+            multiplicar *= j
+        print(multiplicar)
+
+    def divide(self, numeros):
+        a = []
+        for i in numeros:
+            if i != numeros[0]:
+                a.append(int(i))
+        print(a)
+        divides = a[0]
+        for j in a[1:]:
+            if j != 0:
+                divides /= j
+            else:
+                print('Division by zero is not allowed')
+        print(divides)
+
+if __name__ == "__main__":
+    try:
+        fichero = sys.argv[1]
+        with open(fichero, newline='') as fich:
+            lines = fich.readlines()
+            for line in lines:
+                palabras = line.split(',')
+                x = CalculadoraHija(palabras)
+                operacion = palabras[0]
+                if operacion == 'suma':
+                    x.suma(palabras)
+                elif operacion == 'resta':
+                    x.resta(palabras)
+                elif operacion == 'multiplica':
+                    x.multiplica(palabras)
+                elif operacion == 'divide':
+                    x.divide(palabras)
+
+    except:
+        sys.exit("Error")
