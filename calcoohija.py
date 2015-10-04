2e751594cb5b5346a5bc3f7e1c89305dc0b0e4be
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-10-04 13:14:28 +0200

    Update calcoohija.py

--- calcoohija.py
+++ calcoohija.py
@@ -3,46 +3,44 @@
 
 import sys
 
-class Calculadora():
-	def __init__(self, num1, num2):
-		self.num1 = num1
-		self.num2 = num2
 
-	def suma(self, num1, num2):
-		return num1 + num2 
-		
-	def resta(self, num1, num2):
-		return num1 - num2
+class Calculadora():
+    def __init__(self, numero1, numero2):
+        self.numero1 = numero1
+        self.numero2 = numero2
+
+    def suma(self, sumando1, sumando2):
+        return sumando1 + sumando2
+
+    def resta(self, numero1, numero2):
+        return numero1 - numero2
+
 
 class CalculadoraHija(Calculadora):
+    def multiplica(self, numero1, numero2):
+        return numero1 * numero2
 
-	def multiplica(self, num1, num2):
-		return num1*num2	
-
-	def divide(self, num1, num2):
-		return num1 / num2
+    def divide(self, numero1, numero2):
+        return numero1 / numero2
 
 if __name__ == "__main__":
-	
-	try:
-		numero1 = float(sys.argv[1])
-		operacion = str(sys.argv[2])
-		numero2 = float(sys.argv[3])
-		x = CalculadoraHija(numero1, numero2)
+    try:
+        numero1 = float(sys.argv[1])
+        operacion = str(sys.argv[2])
+        numero2 = float(sys.argv[3])
+        x = CalculadoraHija(numero1, numero2)
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    except ZeroDivisionError:
+        sys.exit("Division by zero is not allowed")
 
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-	except ZeroDivisionError:
-		sys.exit("Division by zero is not allowed")
-
-	if operacion == "suma":
-		print(x.suma(numero1, numero2))
-	elif operacion == "resta":
-		print(x.resta(numero1, numero2))
-	elif operacion == "multiplica":
-		print(x.multiplica(numero1, numero2))
-	elif operacion == "divide":
-		print(x.divide(numero1, numero2))
-	else:
-		print("Operación incorrecta")
-
+    if operacion == "suma":
+        print(x.suma(numero1, numero2))
+    elif operacion == "resta":
+        print(x.resta(numero1, numero2))
+    elif operacion == "multiplica":
+        print(x.multiplica(numero1, numero2))
+    elif operacion == "divide":
+        print(x.divide(numero1, numero2))
+    else:
+        print("Operación incorrecta")
