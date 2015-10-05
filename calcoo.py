16a4b6107949008291a07c6da687848e7d6008fd
wendy carolina iza gualiche <wendy@zeta40.aulas.gsyc.es>
2015-10-05 10:49:10 +0200

    calcoo final

--- calcoo.py
+++ calcoo.py
@@ -18,15 +18,12 @@ class Calculadora():
 if __name__ == "__main__":
     try:
         numero1 = float(sys.argv[1])
-        operacion = str(sys.argv[2])
         numero2 = float(sys.argv[3])
         x = Calculadora(numero1, numero2)
-
+        dicc = {"sumar": x.suma, "restar": x.resta}
+        operacion = dicc[sys.argv[2]]
+        print(operacion(numero1,numero2))
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-    if operacion == "suma":
-        print(x.suma(numero1, numero2))
-    elif operacion == "resta":
-        print(x.resta(numero1, numero2))
-    else:
-        print("Operación incorrecta")
+    except KeyError:
+        sys.exit("Operación incorrecta")
